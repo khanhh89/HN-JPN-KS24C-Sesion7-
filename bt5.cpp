3a622873ccae9d4b16679c17b72646084378ebed
khanhh89 <khanhabcabc0000@gmail.com>
2024-11-29 00:19:01 +0700

    Add files via upload

---
+++ bt5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int arr[5] = {8, 15, 2, 99, 34};
+    int max = arr[0], min = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Phan tu lon nhat trong mang la: %d\n", max);
+    printf("Phan tu nho nhat trong mang la: %d\n", min);
+
+    return 0;
+}
